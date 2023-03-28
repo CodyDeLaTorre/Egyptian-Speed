7d48b46845304cc84b5cd78e42b7a47c0278ad46
Cody De La Torre <103235685+CodyDeLaTorre@users.noreply.github.com>
2023-03-28 09:45:00 -0600

    Create player.py

---
+++ utils/player.py
@@ -0,0 +1,17 @@
+from .card import Card, Deck
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    def draw(self, deck):
+        self.hand.append(deck.draw_card())
+        return self
+
+    def showHand(self):
+        for card in self.hand:
+            card.show()
+
+    def draw_card(self):
+        return self.hand.pop().show()
