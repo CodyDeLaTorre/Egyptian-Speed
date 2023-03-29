a6dde9c668ead132650c794b41c6a5dc309d45d5
Cody De La Torre <103235685+CodyDeLaTorre@users.noreply.github.com>
2023-03-29 09:49:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils.card import Deck
 from utils.player import Player
+import re
 
 # rules: 
 # You win by ending up with the whole deck
@@ -21,6 +22,7 @@ class Game():
         self.deck = Deck()
         self.bot = Player('bot')
         self.player = Player('player')
+        self.times_to_place = 0
 
         self.intro()
         self.game_loop()
@@ -36,33 +38,83 @@ class Game():
         print("bot: I'll start")
 
     def game_loop(self):
+        placed_cards = []
         if self.bot_turn_tracker == True:
-            placed_card = self.bot.place_card()
+            print("Bots turn")
+            self.times_to_place +=1
+            while self.times_to_place > 0:
+                placed_cards.append(self.bot.place_card())
+                self.lose_check()
+                self.times_to_place -= 1
         elif self.bot_turn_tracker == False:
-            placed_card = self.player.place_card()
-        self.pot.append(placed_card)
-        # print(placed_card)
-        self.turn_decision(placed_card)
-        print(self.pot)
+            print("Players turn")
+            self.times_to_place +=1
+            while self.times_to_place > 0:
+                placed_cards.append(self.player.place_card())
+                self.lose_check()
+                self.times_to_place -= 1
+        for i in placed_cards:
+            self.pot.append(i)
+            self.turn_decision(i)
         
     def turn_decision(self,card):
-        str(card)
-        print(type(card))
-        print(card)
-        result = self.has_numbers(card)
+        string_card = str(card)
+        result = self.has_numbers(string_card)
         if result == True:
             self.flip_turn()
-    
+            self.game_loop()
+        else:
+            ace_result = self.ace_match(string_card)
+            king_result = self.king_match(string_card)
+            queen_result = self.queen_match(string_card)
+            jack_result = self.jack_match(string_card)
+            if ace_result == True:
+                self.times_to_place += 3
+                self.flip_turn()
+                self.game_loop()
+            elif king_result == True:
+                self.times_to_place += 2
+                self.flip_turn()
+                self.game_loop()
+            elif queen_result == True:
+                self.times_to_place +=1
+                self.flip_turn()
+                self.game_loop()
+            elif jack_result == True:
+                self.flip_turn()
+                self.game_loop()
+
+    def lose_check(self):
+        if len(self.bot.hand) == 52 or len(self.player.hand) == 0:
+            print("Sorry, you lost I win")
+            exit()
+        elif len(self.player.hand) == 52 or len(self.bot.hand) ==0:
+            print("NICE!! YOU WON")
+            exit()
+        else:
+            return
+            
     def flip_turn(self):
         if self.bot_turn_tracker == True:
             self.bot_turn_tracker = False
-        else:
+        elif self.bot_turn_tracker == False:
             self.bot_turn_tracker = True
 
-    def has_numbers(inputString):
-        return bool(char.isdigit() for char in inputString)
+    def has_numbers(self,inputString):
+        return any(char.isdigit() for char in inputString)
+    
+    def ace_match(self,inputString):
+        return bool(re.search("Ace", inputString))
 
+    def king_match(self,inputString):
+        return bool(re.search("King", inputString))
 
+    def queen_match(self,inputString):
+        return bool(re.search("Queen", inputString))
+
+    def jack_match(self,inputString):
+        return bool(re.search("Jack", inputString))
+    
 
 if __name__ == "__main__":
     game = Game()
